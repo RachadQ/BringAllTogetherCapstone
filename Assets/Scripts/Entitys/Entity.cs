5a582b2e790f59021e5b4182af0801e308955771
JazzQ <rachadquintyne@gmail.com>
2019-04-03 01:08:12 -0400

    Revert "Update Enemy Player"
    This reverts commit 24bd5f93af651d98f80fed2852899d3168b96be3.

--- Assets/Scripts/Entitys/Entity.cs
+++ Assets/Scripts/Entitys/Entity.cs
@@ -1,117 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Linq;
-using System;
-using Random = System.Random;
 
 public class Entity : InteractObject
 {
-    public virtual uint UID { get; set; }
-    public CombatStats combatStats;
-    public virtual int AttackSpeed { get { return combatStats.AttackSpeed; } }
-    public virtual int AttackRange { get { return Math.Max(2, combatStats.AttackRange); } }
-
-    public virtual byte Xp { get; set; }
-    public virtual string EntityName { get; set; }
-    public virtual int Level { get; set; }
-
-   
- 
- 
-    public virtual int Mana { get; set; }
-    public virtual int Stamina { get; set; }
-
-    public virtual int MaximumLife { get;  }
-    public virtual int CurrentLife { get; set; }
-    public bool Alive { get { return CurrentLife > 0; } }
-
-    public virtual int MaximumMana { get { return combatStats.MaxMana; } }
-
-    internal bool sentDeath = false;
-
-    public virtual ulong CalculateExpGain(Entity _target, uint _dmg)
+    public void hello()
     {
-
-        // if it is player dont get exp 
-        if (_target is Player)
-            return 0;
-        //get exp depending if target is alive or dmg
-
-        var exp = (double)Math.Min(_target.CurrentLife, _dmg);
-        //get Level difference
-        var levelDifference = Level - _target.Level;
-        if (levelDifference >= 3)//Weak Monsters
-        {
-            if (levelDifference >= 3 && levelDifference <= 5)
-                exp *= .7;
-            else if (levelDifference > 5 && levelDifference <= 10)
-                exp *= .2;
-            else if (levelDifference > 10 && levelDifference <= 20)
-                exp *= .1;
-            else if (levelDifference > 20)
-                exp *= .05;
-        }
-
-        else if (levelDifference < -15) // super stronger (black Name)
-            exp *= 1.8;
-        else if (levelDifference < -8) // strong (red name)
-            exp *= 1.5;
-        else if (levelDifference < -5) // close to level (white name)
-            exp *= 1.3;
-
-        return (ulong)exp;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
     }
 
-    public uint CalculatePhysicalDamage(Entity _target, bool _canDodge = false)
+    // Update is called once per frame
+    void Update()
     {
-        System.Random rnd = new Random();
-        var dmg = rnd.Next(combatStats.MinimumDamage, combatStats.MaximumDamage);
-        if (_target is Monster)
-        {
-            //check if player is 3 levels higher then target
-            dmg *= GetLevelBonus(Level, _target.Level);
-        }
-
-        dmg -= _target.combatStats.Defense;
-        if (dmg < 1)
-            dmg = 1;
-        //if (_canDodge)
-        //   dmg = 0;
-        Debug.Log("player Damage: " + dmg + " attckers level: " + Level + " object attcked level: " + _target.Level);
-        //   ulong exp = CalculateExperienceGain(_target, (uint)dmg);
-
-        return (uint)dmg;
-    }
-
-
-    public int GetLevelBonus(int l1, int l2)
-    {
-        int num = l1 - l2;
-        int bonus = 1;
-        if (num >= 3)
-        {
-            bonus = 2 + num / 3;
-        }
-        return bonus;
-    }
-
-    public virtual void RecieveDamage(uint _dmg)
-    {
-        // if object is alive
-        if (Alive)
-        {
-            // check if life is greater then damage delt
-            if (CurrentLife > _dmg)
-            {
-                // do damage
-                CurrentLife -= (ushort)_dmg;
-            }
-            else
-            {
-                //sett player health to 0
-                CurrentLife = 0;
-            }
-        }
+        
     }
 }
--- Assets/Scripts/Entitys/Player.cs
+++ Assets/Scripts/Entitys/Player.cs
@@ -4,58 +4,24 @@ using UnityEngine;
 
 public class Player : Entity
 {
-   public PlayerMovement movement;
-    public CameraController cam;
-    // character personal stats;
-    public PC Character;
-    
-    //Player Personal info
-    public override int MaximumLife => combatStats.MaxLife + Character.Life;
-    public override int CurrentLife { get => base.CurrentLife; set => base.CurrentLife = value; }
-    public override string EntityName { get { return Character.Name; } set { Character.Name = value; } }
-
-    public override int Level
-    { get { return Character.Level; } set { Character.Level = value; Character.Level = value; } }
-    public int Money { get { return Character.Money; } set { Character.Money = value; } }
-    public int BankMoney { get { return Character.BankMoney; } set { Character.BankMoney = value; } }
-    public int Gem { get { return Character.Gem; } set { Character.Gem = value; } }
-    public int Experience { get { return Character.Experience; } set { Character.Experience = value; } }
-    public int Strength { get { return Character.Strength; } set { Character.Strength = value; } }
-    public int Vitality { get { return Character.Vitality; } set { Character.Vitality = value; } }
-    public int Agility { get { return Character.Agility; } set { Character.Agility = value; } }
-    public int Intellect { get { return Character.Intellect; } set { Character.Intellect = value; } }
-    public int ExtraStats { get { return Character.ExtraStats; } set { Character.ExtraStats = value; } }
-
-
-
+    PlayerMovement movement;
+    CameraController cam;
     // Start is called before the first frame update
     void Start()
     {
-        Level = 1;
-        CurrentLife = MaximumLife;
+       
         movement = new PlayerMovement(this.gameObject);
         cam = new CameraController(this.transform);
-        Debug.Log(CurrentLife);
     }
 
     // Update is called once per frame
-   void Update()
+    void Update()
     {
        
-        
-        
-      
     }
     private void FixedUpdate()
     {
         cam.CameraMovement();
         movement.MovePlayer();
     }
-
-    public void damage(int amount)
-    {
-        CurrentLife -= amount;Debug.Log(CurrentLife);
-    }
-
-
 }
